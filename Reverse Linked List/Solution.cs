8abe08420dbea27671dc6256af8b42c5fbc01a06
Leonardo Alves <leonardo.alves@eritel.com.br>
2022-06-23 13:27:35 -0300

    Add project files.

---
+++ Reverse Linked List/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Reverse_Linked_List
+{
+    public class Solution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+
+            while (head != null)
+            {
+                ListNode next_node = head.next;
+                head.next = prev;
+                prev = head;
+                head = next_node;
+            }
+
+            return prev;
+        }
+    }
+}
